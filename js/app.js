1b5d253961771893c1e6831aaa9478adef29d76b
vjespinoza <vjespinoza84@gmail.com>
2021-09-06 15:51:58 -0500

    comments added

--- js/app.js
+++ js/app.js
@@ -4,12 +4,18 @@ let results = document.getElementById("results");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    //Clears results div before new request
     cleanResults();
+
     let query = input.value;
+
+    //Performs a request based the pokémon's name or ID, after validating that the input is not empty.
     query &&
         axios
             .get(`https://pokeapi.co/api/v2/pokemon/${query}`)
             .then((res) => {
+                //Get the types of the queried pokémon
                 input.value = "";
                 let name = res.data.name;
                 let types = res.data.types.map((t) => {
@@ -24,18 +30,27 @@ form.addEventListener("submit", (e) => {
                     type_a: [],
                     type_b: [],
                 };
+
                 let req1 = axios.get(
                     `https://pokeapi.co/api/v2/type/${types[0]}`
                 );
+
                 let req2 = types[1]
                     ? axios.get(`https://pokeapi.co/api/v2/type/${types[1]}`)
                     : null;
+
+                //Prepares the request array based on the number of types the pokémon has
                 let requestArray = types[1] ? [req1, req2] : [req1];
 
+                //Conditionally performs 1 or 2 requests depending on how many types the pokémon has
+                //The rquests are made to the /type endpoint in order to obtain the damage_relations object.
                 axios
                     .all(requestArray)
                     .then(
                         axios.spread((...responses) => {
+                            //Prepares the damageByType object in order to feed the calculateWeakness function down below
+                            //It creates 1 or 2 arrays (type_a and type_b) based on how many types the pokémon has.
+                            //Each array contains objects with the name of the type/damage relation and the value of the damage
                             let res1 = responses[0];
                             let res2 = responses[1] ? responses[1] : null;
 
@@ -79,6 +94,7 @@ form.addEventListener("submit", (e) => {
                                       ),
                                   ]
                                 : [];
+
                             return damageByType;
                         })
                     )
@@ -97,15 +113,20 @@ form.addEventListener("submit", (e) => {
             });
 });
 
+//calculate the weaknesses based on the damage relations pull from the API
 const calculateWeakness = (types) => {
     let { name, type_a, type_b } = types;
     let commonTypes = [];
     let uncommonTypes = [...type_a, ...type_b];
 
+    //Iterates over the arrays to find common type names
     type_a.map((x) => {
-        type_b.map((y, i) => {
+        type_b.map((y) => {
             if (Object.values(x).includes(Object.values(y)[0])) {
+                //If both arrays share types, the values are multiplied and they get pushed to commonTypes
                 commonTypes.push({ name: y.name, value: x.value * y.value });
+
+                //The remaning types are filtered out of uncommonTypes
                 uncommonTypes = uncommonTypes.filter((z) => {
                     return z.name !== x.name;
                 });
@@ -113,6 +134,7 @@ const calculateWeakness = (types) => {
         });
     });
 
+    //commonTypes and uncommonTypes are concatenated and then the weaknesses with values equal o higher to 2 are filtered
     let weakness = [...commonTypes, ...uncommonTypes].filter((w) => {
         return w.value >= 2;
     });
@@ -122,6 +144,7 @@ const calculateWeakness = (types) => {
     return weakness;
 };
 
+//Writes the resulting weaknesses to the document
 const writeResults = (name, weakness) => {
     let header = document.createElement("h2");
     header.innerHTML = `<span>${name}</span> is weak against: `;
@@ -143,6 +166,7 @@ const writeResults = (name, weakness) => {
     });
 };
 
+//Writes error message to the document
 const notifyError = () => {
     let error = document.createElement("h2");
     error.textContent = "Incorrect name or ID, please try again...";
@@ -150,10 +174,12 @@ const notifyError = () => {
     results.appendChild(error);
 };
 
+//Clears the results div for new request to be written
 const cleanResults = () => {
     results.innerHTML = "";
 };
 
+//Fills the background of each weakness (listItem) with the proper color.
 const colorizer = (type) => {
     let pokeTypeColors = {
         bug: ["#729f3f", "#729f3f"],
